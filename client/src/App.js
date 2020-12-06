599acb1a04ce694f991663b8a64313c8a1701ce8
ALKIS KOUDOUNAS s278266 <s278266@studenti.polito.it>
2020-12-06 17:44:07 +0100

    First implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,75 @@
 import React from 'react';
 import './App.css';
 
+import NavBar from './Components/NavBar';
+
+
+// Import react-router-dom
+import { BrowserRouter as Router, Route, Switch, Redirect, } from 'react-router-dom';
+
+
 class App extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { 
+            location: 'home', 
+            errorMsg: '',      // Error message received by an API call
+        }
+    }
+
+    componentDidMount() {}
+
+    /*loadInitialData() {
+        API.api_name
+          .then() 
+          .catch((errorObj) => this.handleErrors(errorObj));
+      }
+
+    handleErrors = (errorObj) => {
+        if (errorObj) {
+          const err0 = errorObj.errors[0];
+          const errorString = err0.param + ': ' + err0.msg;
+          this.setState({ errorMsg: errorString });
+        }
+      }
+    */
+    setLocation = (location) => {
+        this.setState({ location: location });
+    }
+
+    render() {
+        return (
+            <>
+            <Router>
+            {/* <Container style={{backgroundImage: 'url(/svg/background.svg)'}} fluid> */}
+
+
+            <Switch>
+
+              <Route path="/" render={(props) => {
+                return (
+                    <NavBar location={this.state.location} setLocation={this.setLocation}/>
+                );
+              }} />
+
+              <Route path='/recipe' render={(props) => {}} >
+              </Route>
+
+
+              <Route path="/last_recipe"> </Route>
+
+              <Route path="/favorite_recipes"> </Route>
+
+              <Route path="/last_ingredients"> </Route>
+
+            </Switch>
+
+          {/* </Container> */}
+        </Router>
+      </>
+        );
+      }
 }
 
 
---
+++ client/src/Components/NavBar.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+
+import { Link, Redirect } from 'react-router-dom';
+
+
+class NavBar extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            destination: ''
+        }
+    }
+    
+
+    render() {
+        return (
+            <div className="container-fluid">
+                <Navbar bg="white" variant="white" expand="sm" fixed="top" className="row d-flex justify-content-between">
+
+                    {this.props.location === 'house' 
+                    ? 
+                    <>
+                        <Navbar.Brand href="/" className="ml-2">
+                            <Image src="logo.jpg" width={100}/> 
+                        </Navbar.Brand>
+                        <Nav>
+                            <i className="fas fa-user mr-2" style={{ fontSize: 40, color: '#f3a42d' }} />
+                        </Nav>
+                    </>
+                    : 
+                    <>
+                        <Navbar.Brand href="/" className="ml-2 font_text" /*onClick={this.props.setLocation()}*/>
+                            <i className="fas fa-angle-left" style={{ color: 'rgba(0, 0, 0)' }}/>
+                            <span className="ml-1">{this.props.location}</span>
+                        </Navbar.Brand>
+                           
+                        <Nav.Item href="/" className="ml-2 " /*onClick={this.props.setLocation()}*/>
+                            <a className="mr-2 font_text font-weight-bold"><span>Label</span></a>
+                        </Nav.Item>
+                    </>
+                    }
+
+                    
+
+                </Navbar>
+            </div>
+        );
+    } 
+}
+
+
+export default NavBar;
