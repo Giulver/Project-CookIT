7ffc5ae56ab7231dd6ba79afea1e28e67041fe18
ALKIS KOUDOUNAS s278266 <s278266@studenti.polito.it>
2020-12-10 13:06:52 +0100

    Modified recipes caption

--- client/src/Components/RecipeElement.js
+++ client/src/Components/RecipeElement.js
@@ -5,7 +5,7 @@ import Image from 'react-bootstrap/Image';
 
 import { Link, Redirect } from 'react-router-dom';
 import {Row, Card} from 'react-bootstrap';
-import Recipe from '../api/Recipe';
+//import Recipe from '../api/Recipe';
 
 
 class RecipeElement extends React.Component {
@@ -18,11 +18,11 @@ class RecipeElement extends React.Component {
     render() {
         return (
            
-                <Card className="bg-dark text-white">
+                <Card>
                     <Link>
                      <Card.Img src={this.props.recipe.cover} alt="Card image" />
                     <Card.ImgOverlay>
-                        <Card.Footer>{this.props.recipe.name}</Card.Footer>
+                        <Card.Title className="caption_recipes font_text">{this.props.recipe.name}</Card.Title>
                     </Card.ImgOverlay>
                     </Link>
                 </Card>
